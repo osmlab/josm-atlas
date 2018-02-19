be27819aaf56a3a222bc550885d3a5b75a4014f0
Vincent Privat <vincent.privat@gmail.com>
2018-02-19 01:23:41 +0100

    update to JOSM 13440+ to use the read-only data layer feature. Greatly improves usability of Atlas plugin by reusing most of JOSM features

--- src/main/java/org/openstreetmap/atlas/AtlasReaderLayer.java
+++ src/main/java/org/openstreetmap/atlas/AtlasReaderLayer.java
@@ -1,40 +1,40 @@
 package org.openstreetmap.atlas;
 
-import java.awt.Graphics2D;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
 import javax.swing.Action;
 import javax.swing.Icon;
 
 import org.openstreetmap.atlas.geography.atlas.Atlas;
-import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
-import org.openstreetmap.josm.data.osm.visitor.paint.MapRendererFactory;
-import org.openstreetmap.josm.data.osm.visitor.paint.Rendering;
-import org.openstreetmap.josm.gui.MapView;
-import org.openstreetmap.josm.gui.layer.AbstractModifiableLayer;
 import org.openstreetmap.josm.gui.layer.Layer;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Logging;
 
 /**
  * @author jgage
  */
-public class AtlasReaderLayer extends AbstractModifiableLayer
+public class AtlasReaderLayer extends OsmDataLayer
 {
-    private static String LAYER_NAME = "Atlas Layer";
+    private static final String LAYER_NAME = tr("Atlas Layer");
 
     private Atlas atlas;
-    private final DataSet dataSet;
     private final Bounds bounds;
 
     public AtlasReaderLayer(final String info, final DataSet data, final Atlas atlas,
             final Bounds bounds)
     {
-        super(info);
-        this.dataSet = data;
+        super(data, info, null);
         this.atlas = atlas;
         this.bounds = bounds;
+        // Make sure dataset is read-only
+        if (!data.isReadOnly())
+        {
+            data.setReadOnly();
+        }
     }
 
     @Override
@@ -50,13 +50,13 @@ public class AtlasReaderLayer extends AbstractModifiableLayer
 
     public DataSet getData()
     {
-        return this.dataSet;
+        return this.data;
     }
 
     @Override
     public Icon getIcon()
     {
-        return ImageProvider.get("dialogs", "world");
+        return new ImageProvider("dialogs/world").setSize(ImageProvider.ImageSizes.LAYER).get();
     }
 
     @Override
@@ -78,46 +78,9 @@ public class AtlasReaderLayer extends AbstractModifiableLayer
     }
 
     @Override
-    public boolean isMergable(final Layer arg0)
+    public boolean isMergable(final Layer other)
     {
         return false;
-    }
-
-    /**
-     * Atlas should be treated as immutable.
-     *
-     * @return Always false
-     */
-    @Override
-    public final boolean isModified()
-    {
-        return false;
-    }
-
-    /**
-     * We should not be able to merge layers.
-     *
-     * @param layer
-     *            The layer parameter is ignored
-     */
-    @Override
-    public void mergeFrom(final Layer layer)
-    {
-    }
-
-    /**
-     * Paints the dataSet comprised of the OSM equivalents of atlas objects.
-     */
-    @Override
-    public void paint(final Graphics2D graphics, final MapView mapView, final Bounds bounds)
-    {
-        final boolean active = mapView.getLayerManager().getActiveLayer() == this;
-        final boolean inactive = !active && Main.pref.getBoolean("draw.data.inactive_color", true);
-        final boolean virtual = !inactive && mapView.isVirtualNodesEnabled();
-        Main.pref.getBoolean("draw.data.inactive_color", false);
-        final Rendering painter = MapRendererFactory.getInstance().createActiveRenderer(graphics,
-                mapView, inactive);
-        painter.render(this.dataSet, virtual, bounds);
     }
 
     @Override
