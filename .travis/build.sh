4f3c6488326831f2d733c5e146f5cd1c22b2239d
Florian Schäfer <florian@schaeferban.de>
2018-03-08 18:56:53 +0100

    Publish releases on GitHub, push translatable strings to Transifex (#13)
    Looks Great! Thank you for your valuable contribution. My apologies for taking so long to merge this.

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,19 +2,4 @@
 
 chmod u+x gradlew
 
-if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
-then
-	# This is a release job, triggered manually
-	# Change the version locally to remove the -SNAPSHOT
-	sed -i "s/-SNAPSHOT//g" gradle.properties
-	echo "This is a manual release!"
-else
-	echo "Not a manual release"
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ];
-then
-	./gradlew clean build minJosmVersionClasses
-else
-	./gradlew clean build minJosmVersionClasses
-fi
+./gradlew clean build minJosmVersionClasses
